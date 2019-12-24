439eee186b5e5b235f9e7929a1822b8ca3adeba6
DJDJ326 <djin326@hanmail.net>
2019-12-24 17:46:55 +0900

    결제 폼 완성

--- src/main/java/com/santabooks/www/SubscribeController.java
+++ src/main/java/com/santabooks/www/SubscribeController.java
@@ -2,11 +2,18 @@ package com.santabooks.www;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class SubscribeController {
 
-	@RequestMapping(value="/subscribe/main")
-	public void main() {}
+	@RequestMapping(value="/subscribe/agree")
+	public void main() {}	
+
+	@RequestMapping(value="/subscribe/first")
+	public void pay() {}	
+	
+	@RequestMapping(value="/pay/complete", method=RequestMethod.POST)
+	public void payComplete( ) { }
 	
 }
