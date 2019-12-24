6d3cf7282f02350b8250e12bc11ef3da321b10c3
dohun <mindo413@naver.com>
2019-12-24 16:20:47 +0900

    리뷰 리스트 작업중

--- src/main/java/com/santabooks/reviewSns/dao/face/ReviewSnsDao.java
+++ src/main/java/com/santabooks/reviewSns/dao/face/ReviewSnsDao.java
@@ -1,5 +1,24 @@
 package com.santabooks.reviewSns.dao.face;
 
+import java.util.List;
+
+import com.santabooks.reviewSns.dto.ReviewSns;
+import com.santabooks.util.Paging;
+
 public interface ReviewSnsDao {
+	
+	/**
+	 * 리뷰 총 개수 조회
+	 * 
+	 * @return int - 게시물 총 개수
+	 */
+	public int selectCntAll();
+	
+	/**
+	 * 리뷰리스트 전체 조회
+	 * @param reviewPaging
+	 * @return
+	 */
+	public List<ReviewSns> selectAll(Paging reviewPaging);
 
 }
--- src/main/java/com/santabooks/reviewSns/service/impl/ReviewSnsServiceImpl.java
+++ src/main/java/com/santabooks/reviewSns/service/impl/ReviewSnsServiceImpl.java
@@ -1,5 +1,30 @@
 package com.santabooks.reviewSns.service.impl;
 
-public class ReviewSnsServiceImpl {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.santabooks.reviewSns.dao.face.ReviewSnsDao;
+import com.santabooks.reviewSns.dto.ReviewSns;
+import com.santabooks.reviewSns.service.face.ReviewSnsService;
+import com.santabooks.util.Paging;
+
+@Service
+public class ReviewSnsServiceImpl implements ReviewSnsService{
+	
+	@Autowired ReviewSnsDao reviewSnsDao;
+
+	@Override
+	public int selectCntAll() {
+		
+		return reviewSnsDao.selectCntAll();
+	}
+
+	@Override
+	public List<ReviewSns> snsList(Paging reviewPaging) {
+		
+		return reviewSnsDao.selectAll(reviewPaging);
+	}
 
 }
