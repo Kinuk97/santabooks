b2e5d4e06f7602fe0210c39066d059cbe0fdddaf
dohun <mindo413@naver.com>
2019-12-24 14:43:03 +0900

    ReviewSns 구조 만들기

--- src/main/java/com/santabooks/www/ReviewSnsController.java
+++ src/main/java/com/santabooks/www/ReviewSnsController.java
@@ -1,13 +1,25 @@
 package com.santabooks.www;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.santabooks.reviewSns.service.face.ReviewSnsService;
+import com.santabooks.util.Paging;
+
 
 @Controller
 public class ReviewSnsController {
 	
+	private static final Logger logger = LoggerFactory.getLogger(ReviewSnsController.class);
+	
+	@Autowired private ReviewSnsService reviewSnsService;
+	
 	@RequestMapping(value="/sns/list")
-	public void snsList() {
+	public void snsList(Model model, Paging paging) {
 		
 	}
 }
