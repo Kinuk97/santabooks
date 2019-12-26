1c5e3750310a2d03190b18e153d86d4b882567c1
dohun <mindo413@naver.com>
2019-12-26 19:14:09 +0900

    리뷰 상세보기

--- src/main/java/com/santabooks/reviewSns/dto/ReviewSns.java
+++ src/main/java/com/santabooks/reviewSns/dto/ReviewSns.java
@@ -8,10 +8,20 @@ public class ReviewSns {
 	private int privacy;
 	private String reviewDate;
 
+	// book
+	private String bookName;
+	private String bookWriter;
+	private String bookPublisher;
+	private String publishingYear;
+	private String bookContent;
+	private int genreNo;
+
 	@Override
 	public String toString() {
 		return "ReviewSns [feedNo=" + feedNo + ", bookNo=" + bookNo + ", memberNo=" + memberNo + ", review=" + review
-				+ ", privacy=" + privacy + ", reviewDate=" + reviewDate + "]";
+				+ ", privacy=" + privacy + ", reviewDate=" + reviewDate + ", bookName=" + bookName + ", bookWriter="
+				+ bookWriter + ", bookPublisher=" + bookPublisher + ", publishingYear=" + publishingYear
+				+ ", bookContent=" + bookContent + ", genreNo=" + genreNo + "]";
 	}
 
 	public int getFeedNo() {
@@ -60,6 +70,54 @@ public class ReviewSns {
 
 	public void setReviewDate(String reviewDate) {
 		this.reviewDate = reviewDate;
+	}
+
+	public String getBookName() {
+		return bookName;
+	}
+
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+
+	public String getBookWriter() {
+		return bookWriter;
+	}
+
+	public void setBookWriter(String bookWriter) {
+		this.bookWriter = bookWriter;
+	}
+
+	public String getBookPublisher() {
+		return bookPublisher;
+	}
+
+	public void setBookPublisher(String bookPublisher) {
+		this.bookPublisher = bookPublisher;
+	}
+
+	public String getPublishingYear() {
+		return publishingYear;
+	}
+
+	public void setPublishingYear(String publishingYear) {
+		this.publishingYear = publishingYear;
+	}
+
+	public String getBookContent() {
+		return bookContent;
+	}
+
+	public void setBookContent(String bookContent) {
+		this.bookContent = bookContent;
+	}
+
+	public int getGenreNo() {
+		return genreNo;
+	}
+
+	public void setGenreNo(int genreNo) {
+		this.genreNo = genreNo;
 	}
 
 }
