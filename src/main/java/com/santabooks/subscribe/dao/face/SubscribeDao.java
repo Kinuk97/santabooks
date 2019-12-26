3d082e387881f0bec490ac39b5b5defdb8eaea60
DJDJ326 <djin326@hanmail.net>
2019-12-26 15:13:42 +0900

    구독 진행중

--- src/main/java/com/santabooks/subscribe/dao/face/SubscribeDao.java
+++ src/main/java/com/santabooks/subscribe/dao/face/SubscribeDao.java
@@ -1,5 +1,9 @@
 package com.santabooks.subscribe.dao.face;
 
+import com.santabooks.subscribe.dto.Subscription;
+
 public interface SubscribeDao {
+
+	public void insertSub(Subscription subscription);
 
 }
--- src/main/java/com/santabooks/subscribe/service/face/SubscribeService.java
+++ src/main/java/com/santabooks/subscribe/service/face/SubscribeService.java
@@ -1,5 +1,9 @@
 package com.santabooks.subscribe.service.face;
 
+import com.santabooks.subscribe.dto.Subscription;
+
 public interface SubscribeService {
+
+	public void subscribe(Subscription subscription);
 	
 }
