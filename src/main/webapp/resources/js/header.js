4355af174e59996d79620a4cd05091246d12f069
Kinuk97 <kiukangg@gmail.com>
2019-12-26 10:54:03 +0900

    헤더 기본 틀 완성

---
+++ src/main/webapp/resources/js/header.js
@@ -0,0 +1,26 @@
+/**
+ * 	헤더에 적용할 JS
+ */
+
+$(document).ready(function() {
+    var url = location.href;
+    var arr = url.split("/");
+    
+    let identifier = arr[3];
+    
+    if (arr.length == 4 || identifier == "subscribe") {
+    	// url이 메인이거나 구독이라면
+    	$("#navBar").find("li").eq(0).addClass("active");
+    } else {
+    	
+    	if (identifier == "") {
+    		$("#navBar").find("li").eq(1).addClass("active");
+    		
+    	} else if (identifier == "") {
+    		$("#navBar").find("li").eq(2).addClass("active");
+    	}
+    	
+    }
+
+
+});
