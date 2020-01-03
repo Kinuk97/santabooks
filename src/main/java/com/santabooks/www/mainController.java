c8a6d12fada6eee50226a725335dcd7f78bde1c7
DJDJ326 <djin326@hanmail.net>
2020-01-03 10:42:21 +0900

    수정중

--- src/main/java/com/santabooks/www/mainController.java
+++ src/main/java/com/santabooks/www/mainController.java
@@ -1,13 +1,30 @@
 package com.santabooks.www;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.santabooks.subscribe.dto.Subscription;
+import com.santabooks.subscribe.service.face.SubscribeService;
 
 @Controller
 public class mainController {
 	
+	@Autowired private SubscribeService subscribeservice;
+	
 	@RequestMapping(value="/main")
-	public void main() {
-		
-	}
+	public String main(Model model, Subscription subscription, HttpSession session) {
+
+		if(session.getAttribute("MemberId") != null) {
+			String memberId = (String) session.getAttribute("MemberId");
+			subscription = subscribeservice.getsubNo(memberId);		
+			model.addAttribute("subscription", subscription);
+			} else {
+				return "redirect:/";
+			}
+			return "/main";
+		}
 }
--- src/main/webapp/WEB-INF/views/main.jsp
+++ src/main/webapp/WEB-INF/views/main.jsp
@@ -18,16 +18,21 @@
 	<h1>SantaBooks</h1>
 	<h3>매월 10일, 관심사에 알맞는 추천 책을<br>
 	 랜덤으로 보내드립니다.
-	 <br>월 9,900원 산타북스와 함께하세요.
+	 <br>월 9,900원 산타북스와 함께하세요. 
 	</h3>
 	
+	<c:choose>
+	<c:when test="${0 eq subscription.subNo }">
+		<a class="btn btn-lg btn-secondary btn-block b-btn" id = "btnmain" href="subscribe/agree"  >이용하러가기</a>
+	</c:when>
+	<c:when test="${null eq subscription.subNo }">
+		<a class="btn btn-lg btn-secondary btn-block b-btn" id = "btnmain" href="subscribe/agree"  >이용하러가기</a>
+	</c:when>
+	<c:otherwise>
+		<a class="btn btn-lg btn-secondary btn-block b-btn" id = "btnmain" onclick="location.href='/novel/list'" >이용하러가기</a>
+	</c:otherwise>	
+	</c:choose>
 
-<%-- 	<c:if test="${0 eq subscription.subNo }"> --%>
-<!-- 		<h6>널이다</h6> -->
-<%-- 	</c:if> --%>
-
-
-		<a class="btn btn-lg btn-secondary btn-block b-btn" id = "btnmain" href="subscribe/agree"  onclick="check()">이용하러가기</a>
 	</section>
 	
 	</div>
