b78178fe747c859e99a46a4542d8bd7d220be838
DJDJ326 <djin326@hanmail.net>
2020-01-07 14:04:50 +0900

    배송_장르에 따른 책 배송완료

--- src/main/java/com/santabooks/www/SubscribeController.java
+++ src/main/java/com/santabooks/www/SubscribeController.java
@@ -1,6 +1,7 @@
 package com.santabooks.www;
 
-import java.util.List;
+import java.time.LocalDateTime;
+import java.util.Random;
 
 import javax.servlet.http.HttpSession;
 
@@ -11,15 +12,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import com.santabooks.member.dto.Member;
 import com.santabooks.reviewSns.dto.Book;
+import com.santabooks.subscribe.dto.Shipment;
 import com.santabooks.subscribe.dto.Subscription;
 import com.santabooks.subscribe.service.face.SubscribeService;
 
 @Controller
 public class SubscribeController {
-
 	
 	@Autowired private SubscribeService subscribeservice;
-	
 	
 	@RequestMapping(value="/subscribe/agree")
 	public void main() {}	
@@ -72,29 +72,47 @@ public class SubscribeController {
 	}
 
 	@RequestMapping(value="/subscribe/shipment")
-	public void shipment(Book book, HttpSession session) {
-		// session에 저장되어있는 장르 번호 조회 --> membercontroller 에서 갖고오기
-		// 세션에서 장르번호 받아오기 위위에서 비슷한 로직 있음. 활용해보자.
-//		String id = (String) session.getAttribute("MemberId"); // 세션에서 id 받아오기
+	public void shipment(HttpSession session, Member member, Shipment shipment) {
 		
-		
-		
-		
-		
-		// book에 저장되있는 장르 번호 조회
-//		List<Book> bookNolist = subscribeservice.getbookNo(session.getAttribute("MemberId"));
-//		
-//		System.out.println(bookNolist + "맞지");
-		
+		// id로 member 저장되어있는 장르 번호 조회 --> membercontroller 에서 갖고오기
+		String id = (String) session.getAttribute("MemberId");
+		member = subscribeservice.getGenre(id);
+		String genreNo = member.getGenre();
+		System.out.println(genreNo); // id에 따른 장르 No 확인 완료
 		
 		// 장르 일치에 따른 book 가져오기(1개)
+		Book bookInfo = subscribeservice.getbookgenreNo(genreNo);
+		
+//		System.out.println(bookInfo);
+		System.out.println(bookInfo.getBookName() + "gdgdg");
+		
+		Random rand = new Random();
+		int shipno = 100000000 + rand.nextInt(900000000);
+		System.out.println("운송장 번호 : " +shipno);
+		//shipno(운송장 번호 - 대한통운)
+		
+		// 운송장번호 ; 있는거로 하나 만들기 shipno
+		shipment.setShipNo(shipno);
 		
 		// shipment 테이블에 book 입력
+		shipment.setBookNo(bookInfo.getBookNo());
 		
-		// 운송장번호 생성 및 입력
 		
-		// 나머지 정보 입력
+		// date  = service에서 만들기
+		LocalDateTime date = LocalDateTime.now();
+		shipment.setShipmentDate(date.toString());
 		
+		// memberno = 로그인한얘 no
+		shipment.setMemberNo((int) session.getAttribute("MemberNo"));
+		
+		// subNo = 구독정보 가져오기
+		shipment.setSubNo((int) session.getAttribute("subNo"));
+		
+//		System.out.println(shipment);
+		
+		
+		
+		subscribeservice.insertShipmentInfo(shipment);
 		
 	}
 
